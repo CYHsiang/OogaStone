d74c134c40648ba8e5f2214859aaee647d22c37f
123 <123@jhijj.com>
2022-12-01 17:12:08 +0800

    CCC

--- 專題/js/index.js
+++ 專題/js/index.js
@@ -15,24 +15,24 @@ function CloseModal() {
 }
 
 // 折價券輪播-----------
-$(".responsive").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-    ]
-});
+// $(".responsive").slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//     ]
+// });
 
 // 首頁動畫
 function showImg() {
