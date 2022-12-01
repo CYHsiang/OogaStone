5f3aa79e70a0ab1afd740a0453aa5f604693a170
text1 <text1@ttt.com>
2022-12-01 17:18:54 +0800

    L0518

---
+++ 專題/js/shoppingCart.js
@@ -0,0 +1,68 @@
+// 加號
+function onplus() {
+    var plusEles = document.querySelector('.AddSub .plus')
+    for (var i = 0; i < plusEles.length; i++) {
+        plusEles[i].onclick = function () {
+            var amountEles = document.querySelectorAll('.AddSub .number')
+            amountEles.value++
+           
+            
+
+            // 計算價格
+            var priceEle = document.querySelectorAll('.price span')
+            var price = priceEle.innerHTML
+            price = price.substring(0) 
+            price = Number(price)
+
+            var num = amountEles.value
+            var totalPrice = price * num
+            
+            var totalPices = querySelectorAll('.money .ssubtotal')
+            totalPices.innerHTML = `NT$ ${totalPrice}元`
+
+            var goodstotalPrice = getSum()
+            var goodtotal = document.querySelectorAll('.total span')
+            goodtotal.innerHTML = `${goodstotalPrice+60}`
+
+            // 計算總價
+
+        }
+
+    }
+};
+onplus();
+
+// 減號
+
+function onminus() {
+    var minusEles = document.querySelectorAll('.AddSub .minus')
+    for (var i = 0; i < minusEles.length; i++) {
+        minusEles[i].onclick = function () {
+            var amountEles = document.querySelectorAll('.AddSub .number')
+            amountEles.value--
+        
+
+            // 計算價格
+            var priceEle = document.querySelectorAll('.price span')
+            var price = priceEle.innerHTML
+            price = price.substring(0) 
+            price = Number(price)
+
+            var num = amountEles.value
+            var totalPrice = price * num
+            
+            var totalPices = querySelectorAll('.money .ssubtotal')
+            totalPices.innerHTML = `NT$ ${totalPrice}元`
+
+            var goodstotalPrice = getSum()
+            var goodtotal = document.querySelectorAll('.total span')
+            goodtotal.innerHTML = `${goodstotalPrice+60}`
+
+            // 計算總價
+
+        }
+
+    }
+};
+onminus();
+
